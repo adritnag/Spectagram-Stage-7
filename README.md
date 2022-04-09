f40511ebde8ea8dfab37807dacef449f8577ced9
adritnag <63493982+adritnag@users.noreply.github.com>
2022-04-09 18:36:05 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Spectagram-Stage-7
+# spectagram-stage-7
+project solution for c87
---
+++ config.js
@@ -0,0 +1,10 @@
+import firebase from "firebase"
+export const firebaseConfig = {
+    apiKey: "AIzaSyDK_pzrgAHUDKDS9MP9f-rQy1VZkhFmoIM",
+    authDomain: "spectagram-app-d9404.firebaseapp.com",
+    databaseURL: "https://spectagram-app-d9404-default-rtdb.firebaseio.com",
+    projectId: "spectagram-app-d9404",
+    storageBucket: "spectagram-app-d9404.appspot.com",
+    messagingSenderId: "931222283443",
+    appId: "1:931222283443:web:3a78c2d1ab7b840c56bcc6"
+  };
